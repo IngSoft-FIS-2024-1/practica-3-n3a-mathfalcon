ad37c7611955803abeb5b2d7d5c57f58a8752691
Mathias Falcon <mathfalcondj@gmail.com>
2024-06-10 22:14:01 -0300

    Parte A

--- domain/tests/book.test.js
+++ domain/tests/book.test.js
@@ -1,46 +1,126 @@
-import {describe, it, expect, beforeEach} from '@jest/globals';
-import Book from '../book.js';
+import { describe, it, expect, beforeEach } from "@jest/globals";
+import Book from "../book.js";
 
-describe('Book', () => {
+describe("Book", () => {
   let myBook;
 
   beforeEach(() => {
-    myBook = new Book('Cuentos de la Selva', 'Horacio Quiroga', 350);
+    myBook = new Book("Cuentos de la Selva", "Horacio Quiroga", 350);
   });
 
-  it('return the correct title', () => {
-    expect(myBook.getTitle()).toBe('Cuentos de la Selva');
+  it("return the correct title", () => {
+    expect(myBook.getTitle()).toBe("Cuentos de la Selva");
   });
 
-  it('return the correct author', () => {
-    expect(myBook.getAuthor()).toBe('Horacio Quiroga');
+  it("return the correct author", () => {
+    expect(myBook.getAuthor()).toBe("Horacio Quiroga");
   });
 
-  it('return the correct number of pages', () => {
+  it("return the correct number of pages", () => {
     expect(myBook.getPages()).toBe(350);
   });
 
-  it('check title is a string', () => {
-    expect(() => myBook = new Book(451, 1, 350)).toThrow();
+  it("return the correct number of words", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+    const bookWords = 1500;
+    const bookWithWords = new Book(bookTitle, bookAuthor, bookPages);
+    bookWithWords.setWords(bookWords);
+    expect(bookWithWords.getWords()).toBe(bookWords);
   });
 
-  it('check title is not empty', () => {
-    expect(() => myBook = new Book('', 'Horacio Quiroga', 350)).toThrow();
+  it("throws when trying to get the number of words without setting them", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+    const bookWithWords = new Book(bookTitle, bookAuthor, bookPages);
+    expect(() => bookWithWords.getWords()).toThrow();
   });
 
-  it('check author is a string', () => {
-    // TODO
+  it("check title is a string", () => {
+    expect(() => (myBook = new Book(451, 1, 350))).toThrow();
   });
 
-  it('check page param is a number', () => {
-    // TODO
+  it("check title is not empty", () => {
+    expect(() => (myBook = new Book("", "Horacio Quiroga", 350))).toThrow();
   });
 
-  it('check pages not < 1', () => {
-    // TODO
-  });
-  it('toString()', () => {
-    // TODO
+  it("check author is a string", () => {
+    expect(typeof myBook.getAuthor() === "string");
   });
 
+  it("check it does not allow non-string values as author", () => {
+    expect(() => new Book("Test Title", undefined, 1)).toThrow();
+  });
+
+  it('check it sets author as "Anonimo" if string length is 0', () => {
+    const book = new Book("Test Title", "", 1);
+    expect(book.getAuthor()).toEqual("Anónimo");
+  });
+
+  it("check page param is a number", () => {
+    expect(() => new Book("Test Title", "Test Author", "NotANumber")).toThrow();
+
+    const correctBook = new Book("Test Title", "Test Author", 1);
+
+    expect(typeof correctBook.getPages()).toEqual("number");
+  });
+
+  it("check pages not < 1", () => {
+    expect(() => new Book("Test Title", "Test Author", -1)).toThrow();
+    expect(() => new Book("Test Title", "Test Author", -5)).toThrow();
+    expect(() => new Book("Test Title", "Test Author", -100)).toThrow();
+  });
+
+  it("toString() without words", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+    const bookWithoutWords = new Book(bookTitle, bookAuthor, bookPages);
+
+    expect(bookWithoutWords.toString()).toEqual(
+      `Título: ${bookTitle} Autor: ${bookAuthor} Páginas: ${bookPages}`
+    );
+  });
+
+  it("toString() with words", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+    const bookWords = 1500;
+
+    const bookWithWords = new Book(bookTitle, bookAuthor, bookPages);
+    bookWithWords.setWords(bookWords);
+
+    expect(bookWithWords.toString()).toEqual(
+      `Título: ${bookTitle} Autor: ${bookAuthor} Páginas: ${bookPages} Numero de palabras: ${bookWords}`
+    );
+  });
+
+  it("check setWords only accepts numbers", () => {
+    expect(() => myBook.setWords("NaN")).toThrow();
+  });
+
+  it("check wordsPerPage throws if words is not defined", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+
+    const book = new Book(bookTitle, bookAuthor, bookPages);
+
+    expect(() => book.wordsPerPage()).toThrow();
+  });
+
+  it("check wordsPerPage returns the average of words per page", () => {
+    const bookTitle = "Test";
+    const bookAuthor = "Autor";
+    const bookPages = 15;
+    const bookWords = 1500;
+
+    const book = new Book(bookTitle, bookAuthor, bookPages);
+    book.setWords(bookWords);
+
+    expect(book.wordsPerPage()).toEqual(100);
+  });
 });
--- domain/tests/library.test.js
+++ domain/tests/library.test.js
@@ -1,37 +1,67 @@
-import { describe, it, expect, beforeEach } from '@jest/globals';
-import Library from '../library.js';
-import Book from '../book.js';
+import { describe, it, expect, beforeEach } from "@jest/globals";
+import Library from "../library.js";
+import Book from "../book.js";
 
-describe('Library', () => {
+describe("Library", () => {
   let myLibrary;
 
   beforeEach(() => {
-    myLibrary = new Library('Biblioteca');
+    myLibrary = new Library("Biblioteca");
   });
 
-  it('add a book to the library', () => {
-    myLibrary.addBook('Cuentos de la Selva', 'Horacio Quiroga', 120);
+  it("add a book to the library", () => {
+    myLibrary.addBook("Cuentos de la Selva", "Horacio Quiroga", 120);
     const aBook = myLibrary.getInventory()[myLibrary.getInventory().length - 1];
     expect(aBook).toBeInstanceOf(Book);
-    expect(aBook.getTitle()).toBe('Cuentos de la Selva');
+    expect(aBook.getTitle()).toBe("Cuentos de la Selva");
   });
 
-  it('return the total number of books', () => {
-    myLibrary.addBook('Cuentos de la Selva', 'Horacio Quiroga', 120);
-    myLibrary.addBook('El Hombre que Calculaba', 'Malba Tahan', 286);
+  it("return the total number of books", () => {
+    myLibrary.addBook("Cuentos de la Selva", "Horacio Quiroga", 120);
+    myLibrary.addBook("El Hombre que Calculaba", "Malba Tahan", 286);
     expect(myLibrary.totalBooks()).toBe(2);
   });
 
-  it('set the name of the library', () => {
-    myLibrary.setName('Montevideo');
-    expect(myLibrary.getName()).toBe('Montevideo');
+  it("set the name of the library", () => {
+    myLibrary.setName("Montevideo");
+    expect(myLibrary.getName()).toBe("Montevideo");
   });
 
-  it('throw an error when setting an invalid name', () => {
+  it("throw an error when setting an invalid name", () => {
     expect(() => myLibrary.setName(123)).toThrow();
   });
-  it('throw an error when setting an empty name', () => {
-    // TODO
+  it("throw an error when setting an empty name", () => {
+    expect(() => myLibrary.setName("")).toThrow();
   });
 
+  it("throw an error when calling totalWords with a book in the inventory that does not have words set", () => {
+    const bookWithoutWords = new Book("Test", "Test", 10);
+    const bookWithWords = new Book("Test", "Test", 10);
+    bookWithWords.setWords(10);
+
+    myLibrary.addBookToLibrary(bookWithoutWords);
+    myLibrary.addBookToLibrary(bookWithWords);
+
+    expect(() => myLibrary.totalWords()).toThrow();
+  });
+
+  it("wordsPerPage returns as expected", () => {
+    const bookWithWords = new Book("Test", "Test", 10);
+    const bookWithWords2 = new Book("Test", "Test", 10);
+    const amountOfWords1 = 10;
+    const amountOfWords2 = 40;
+    bookWithWords.setWords(amountOfWords1);
+    bookWithWords2.setWords(amountOfWords2);
+
+    myLibrary.addBookToLibrary(bookWithWords);
+    myLibrary.addBookToLibrary(bookWithWords2);
+
+    expect(myLibrary.totalWords()).toEqual(amountOfWords1 + amountOfWords2);
+  });
+
+  it("throws when addBookToLibrary is called with a non instance of Book", () => {
+    const nonBook = {};
+
+    expect(() => myLibrary.addBookToLibrary(nonBook)).toThrow();
+  });
 });
